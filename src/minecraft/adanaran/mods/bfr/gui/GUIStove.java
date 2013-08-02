ff64ca811ab85b9f241d37e0989916c119ca2f5a
Demitreus <demitreus@gmx.de>
2013-08-02 13:30:03 +0200

    added gui texture

--- src/minecraft/adanaran/mods/bfr/gui/GUIStove.java
+++ src/minecraft/adanaran/mods/bfr/gui/GUIStove.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.inventory.GuiContainer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.inventory.ContainerFurnace;
 import net.minecraft.tileentity.TileEntityFurnace;
+import net.minecraft.util.ResourceLocation;
 
 /**
  * 
@@ -21,6 +22,7 @@ import net.minecraft.tileentity.TileEntityFurnace;
 public class GUIStove extends GuiContainer {
 	
 	private TileEntityStove stoveInventory;
+	public static final ResourceLocation guiTexture = new ResourceLocation("bfr","gui/container/stove.png");
 
 	public GUIStove(InventoryPlayer invPlayer, TileEntityStove tileEntityStove){
         super(new ContainerStove(invPlayer, tileEntityStove));
@@ -31,6 +33,7 @@ public class GUIStove extends GuiContainer {
 	@Override
 	protected void drawGuiContainerBackgroundLayer(float f, int i, int j) {
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        this.mc.func_110434_K().func_110577_a(guiTexture);
         int k = (this.width - this.xSize) / 2;
         int l = (this.height - this.ySize) / 2;
         this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
