01a50aae63086e9683b136f735ebc333a3d42eac
Adanaran <mikeon@t-online.de>
2013-08-05 00:31:02 +0200

    Oen milling gui on mill activation.

--- src/minecraft/adanaran/mods/bfr/blocks/BlockMill.java
+++ src/minecraft/adanaran/mods/bfr/blocks/BlockMill.java
@@ -2,8 +2,10 @@ package adanaran.mods.bfr.blocks;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.world.World;
+import adanaran.mods.bfr.BFR;
 import adanaran.mods.bfr.entities.TileEntityMill;
 
 public class BlockMill extends BlockContainer {
@@ -15,6 +17,16 @@ public class BlockMill extends BlockContainer {
 	@Override
 	public TileEntity createNewTileEntity(World world) {
 		return new TileEntityMill();
+	}
+
+	@Override
+	public boolean onBlockActivated(World world, int i, int j, int k,
+			EntityPlayer entityPlayer, int meta, float par7, float par8,
+			float par9) {
+		if (!world.isRemote) {
+			entityPlayer.openGui(BFR.instance, 2, world, i, j, k);
+		}
+		return true;
 	}
 
 	// TODO Texturen!
