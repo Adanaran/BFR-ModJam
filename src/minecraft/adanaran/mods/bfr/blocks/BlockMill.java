17792f5c46c1093ebff14792733042179efe3bf4
Adanaran <mikeon@t-online.de>
2013-08-05 00:38:06 +0200

    Textures for mill.

--- src/minecraft/adanaran/mods/bfr/blocks/BlockMill.java
+++ src/minecraft/adanaran/mods/bfr/blocks/BlockMill.java
@@ -2,13 +2,20 @@ package adanaran.mods.bfr.blocks;
 
 import net.minecraft.block.BlockContainer;
 import net.minecraft.block.material.Material;
+import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.tileentity.TileEntity;
+import net.minecraft.util.Icon;
 import net.minecraft.world.World;
 import adanaran.mods.bfr.BFR;
 import adanaran.mods.bfr.entities.TileEntityMill;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
 
 public class BlockMill extends BlockContainer {
+
+	@SideOnly(Side.CLIENT)
+	private Icon millTop;
 
 	public BlockMill(int id) {
 		super(id, Material.rock);
@@ -29,6 +36,16 @@ public class BlockMill extends BlockContainer {
 		return true;
 	}
 
-	// TODO Texturen!
+	@Override
+	public void registerIcons(IconRegister par1IconRegister) {
+		this.blockIcon = par1IconRegister.registerIcon("bfr:mill_side");
+		this.millTop = par1IconRegister.registerIcon("bfr:mill_top");
+	}
 
+	@SideOnly(Side.CLIENT)
+	@Override
+	public Icon getIcon(int par1, int par2) {
+		return par1 == 1 ? this.millTop : (par1 == 0 ? this.millTop
+				: this.blockIcon);
+	}
 }
